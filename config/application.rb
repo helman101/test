9eea0bf1b4d90659f5fa7526cfd988faf2c66f4b
helman101 <helmanandresortegon@gmail.com>
2021-06-17 17:11:30 +0200

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module TodosApi
     config.api_only = true
   end
 
-  config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+  config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
       resource(
@@ -48,4 +48,11 @@ module TodosApi
         )
     end
   end
+
+  config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
+    'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept'
+  }
+  
 end
